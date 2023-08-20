0df18543263993d9a09c94e860240bc91fda765d
virkann <virkan90@gmail.com>
2023-08-20 19:22:20 +0300

    edit pages profile

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,9 +1,33 @@
 import React from 'react'
 
+// icons
+import { BsSearch } from "react-icons/bs";
+import {AiOutlineHeart} from "react-icons/ai"
+
 const Home = () => {
   return (
-    <div></div>
-  )
+    <div>
+      <nav className="fixed md:hidden w-full duration-300 top-0 border-b py-[15px] flex justify-between items-center px-[16px] bg-white">
+        <p className="fontLogo text-[24px] cursor-pointer">Instagram</p>
+        <div className="flex items-center gap-[20px]">
+          {/* Search */}
+          <form className="bg-gray-200/70 flex items-center px-[25px]  rounded-[8px] gap-[10px]">
+            <BsSearch className="text-[14px] text-gray-500 " />
+            <input
+              type="text"
+              className="outline-none bg-transparent border-none p-0 mt-[8px]"
+              placeholder="Search"
+            />
+          </form>
+          <div>
+            <AiOutlineHeart className="text-[28px] cursor-pointer" />
+          </div>
+        </div>
+      </nav>
+      sds
+
+    </div>
+  );
 }
 
 export default Home
--- src/pages/profile/Profile.tsx
+++ src/pages/profile/Profile.tsx
@@ -6,22 +6,23 @@ import { AiOutlineDown } from "react-icons/ai";
 import { BsGrid3X3 } from "react-icons/bs";
 import { FaRegBookmark } from "react-icons/fa";
 import { BiUserPin } from "react-icons/bi";
-
+import { AiOutlineSetting } from "react-icons/ai";
 
 // img import
-import profile from "../../assets/img-profile/profile.jpg";
+// import profile from "../../assets/img-profile/profile.jpg";
 import op from "../../assets/img-profile/op.jpg";
 import n from "../../assets/img-profile/n.jpg";
 import n1 from "../../assets/img-profile/n1.jpg";
 import n2 from "../../assets/img-profile/n2.jpg";
 import n3 from "../../assets/img-profile/n3.jpg";
 import n4 from "../../assets/img-profile/n4.jpg";
-import a1 from "../../assets/img-profile/a1.png"
-import a2 from "../../assets/img-profile/a2.png"
-import a3 from "../../assets/img-profile/a3.png"
-import a4 from "../../assets/img-profile/a4.png"
-import a5 from "../../assets/img-profile/a5.png"
-import a6 from "../../assets/img-profile/a6.png"
+import a1 from "../../assets/img-profile/a1.png";
+import a2 from "../../assets/img-profile/a2.png";
+import a3 from "../../assets/img-profile/a3.png";
+import a4 from "../../assets/img-profile/a4.png";
+import a5 from "../../assets/img-profile/a5.png";
+import a6 from "../../assets/img-profile/a6.png";
+import avatar from "../../assets/img-profile/avatar.jpg";
 
 const Profile = () => {
   return (
@@ -38,31 +39,72 @@ const Profile = () => {
           <FiUserPlus className="text-[25px]" />
         </div>
       </div>
-      <div className="flex gap-7 p-[20px_20px]">
+      {/* info 1 */}
+      <div className="flex gap-[24px] md:items-start md:gap-[70px] px-[16px] md:px-[40px] lg:ml-[80px]  pt-[20px]">
+        {/* img */}
         <div>
-          <img src={profile} alt="" className="rounded-full w-[90px]" />
+          <img
+            src={avatar}
+            className="rounded-full w-[75px] h-[75px] md:w-[150px] md:h-[150px]"
+          />
         </div>
+        {/* info md: */}
         <div>
-          <div className="flex gap-4">
-            <h2>profile</h2>
-            <IoIosSettings className="text-[28px]" />
-          </div>
-          <div className="grid grid-cols-2 gap-4 text-center py-[15px]">
-            <div className="bg-[#ccc] rounded-[14px] text-[14px] leading-4 p-[5px_0px]">
-              Редактировать профиль
+          {/* name and buttons */}
+          <div className="flex flex-col md:mt-[10px] md:flex-row justify-end md:justify-start gap-[13px] md:gap-[16px]">
+            <div className="flex items-center gap-[10px]">
+              <p className="cursor-pointer text-[19px]">virkan</p>
+              <AiOutlineSetting className="text-[24px] cursor-pointer md:hidden" />
             </div>
-            <div className="bg-[#ccc] rounded-[14px] text-[14px] leading-4 p-[5px_0px] w-[99px] lg:w-full">
-              Показать архив
+            <div className="flex gap-[8px] md:items-center">
+              <button className="bg-gray-200/70 hover:bg-gray-200 font-[600] text-[14px] px-[20px] md:px-[10px] py-[5px] rounded-[10px]">
+                Редактировать профиль
+              </button>
+              <button className="bg-gray-200/70 hover:bg-gray-200 font-[600] text-[14px] px-[20px] md:px-[10px] py-[5px] rounded-[10px]">
+                Показать архив
+              </button>
+              <AiOutlineSetting className="text-[24px] cursor-pointer hidden md:flex" />
             </div>
           </div>
+          {/* subscribers */}
+          <div className="mt-[15px] hidden text-center py-[8px] md:flex justify-between">
+            <div className="flex md:gap-[5px]">
+              <p className="text-[14px] leading-0 font-[600]">4</p>
+              <p className="text-[14px] leading-0 text-gray-500 md:text-black">
+                Публикаций
+              </p>
+            </div>
+            <div className="flex md:gap-[5px]">
+              <p className="text-[14px] leading-0 font-[600]">272</p>
+              <button className="text-[14px] leading-0 text-gray-500 md:text-black">
+                Подписчики
+              </button>
+            </div>
+            <div className="flex md:gap-[5px]">
+              <p className="text-[14px] leading-0 font-[600]">333</p>
+              <button className="text-[14px] leading-0 text-gray-500 md:text-black">
+                Подписок
+              </button>
+            </div>
+          </div>
+          {/* name */}
+          {/* <p className="mt-[8px] font-[500] text-[14px] hidden md:flex">
+            Virkan Shirinov
+          </p> */}
         </div>
       </div>
+      {/* name */}
+      {/* <p className="mt-[27px] font-[500] md:hidden text-[14px] px-[16px]">
+        Virkan Shirinov
+      </p> */}
+
+      {/* dfdfdf */}
       <div>
-        <div className="p-[20px_30px]">
+        <div className="p-[20px_30px] lg:m-[0px_54px_0px_54px]">
           <h1>КУРСЫ ПРОГРАММИРОВАНИЯ | ДУШАНБЕ</h1>
         </div>
       </div>
-      <div className="flex gap-[20px] p-[20px_20px] text-center">
+      <div className="flex gap-[20px] p-[20px_20px] text-center lg:m-[0px_54px_0px_60px]">
         <div className="flex flex-col justify-center">
           <img src={n1} alt="" className="rounded-full w-[60px]" />
           <p className="text-[14px] mt-[6px]">Address</p>
@@ -88,48 +130,51 @@ const Profile = () => {
           <p className="text-[14px] mt-[6px]">Address</p>
         </div>
       </div>
-      <div className="border-t-[1px] p-[20px_20px] grid grid-cols-3">
+      <div className="border-t-[1px] p-[20px_20px] grid grid-cols-3 lg:m-[0px_54px_0px_54px] lg:hidden">
         <div className="flex flex-col items-center   ">
           <span>228</span>
-          <p>публикаций</p>
+          <p>Публикаций</p>
         </div>
         <div className="flex flex-col items-center   ">
           <span>7 114</span>
-          <p>подписчиков</p>
+          <p>Подписчиков</p>
         </div>
         <div className="flex flex-col items-center   ">
           <span>22</span>
-          <p>подписок</p>
+          <p>Подписок</p>
         </div>
       </div>
-      <div className="border-t-[1px] grid grid-cols-3 py-[10px]">
-        <div className="flex justify-center items-center">
-          <BsGrid3X3 className="text-[24px] " />
+      <div className="border-t-[1px] flex justify-center py-[10px] gap-[50px] items-center lg:m-[0px_54px_0px_54px]">
+        <div className="flex justify-center items-center gap-[5px]">
+          <BsGrid3X3 className="text-[15px] " />
+          <p className="text-[12px]">ПУБЛИКАЦИИ</p>
         </div>
-        <div className="flex justify-center items-center">
-          <FaRegBookmark className="text-[29px]" />
+        <div className="flex justify-center items-center gap-[5px]">
+          <FaRegBookmark className="text-[18px]" />
+          <p className="text-[12px]">СОХРАНЕННОЕ</p>
         </div>
-        <div className="flex justify-center items-center">
-          <BiUserPin className="text-[35px]" />
+        <div className="flex justify-center items-center gap-[5px]">
+          <BiUserPin className="text-[20px]" />
+          <p className="text-[12px]">ОТМЕТКИ</p>
         </div>
       </div>
-      <div className="grid grid-cols-3 py-[10x] gap-[2px] ">
-        <div>
+      <div className="grid grid-cols-3 py-[10x] gap-[2px] m-[0px_54px_0px_54px] ">
+        <div className="flex flex-col items-center">
           <img src={a1} alt="" className="w-full h-full" />
         </div>
-        <div>
+        <div className="flex flex-col items-center">
           <img src={a2} alt="" className="w-full h-full" />
         </div>
-        <div>
+        <div className="flex flex-col items-center">
           <img src={a3} alt="" className="w-full h-full" />
         </div>
-        <div>
+        <div className="flex flex-col items-center">
           <img src={a4} alt="" className="w-full h-full" />
         </div>
-        <div>
+        <div className="flex flex-col items-center">
           <img src={a5} alt="" className="w-full h-full" />
         </div>
-        <div>
+        <div className="flex flex-col items-center">
           <img src={a6} alt="" className="w-full h-full" />
         </div>
       </div>
