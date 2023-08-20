9e364c55af0f5cc5ee47b785ab9561a0f33c1b69
virkann <virkan90@gmail.com>
2023-08-20 10:11:38 +0300

    button menu

--- src/Layout/Layout.tsx
+++ src/Layout/Layout.tsx
@@ -20,80 +20,84 @@ const Layout = () => {
   return (
     <div>
       {/* Navbar */}
-      <div className="navbar border-[1px] fixed top-0 left-0 md:w-[260px] flex flex-col justify-between h-screen   ">
-        <ul className=" p-[40px_25px_5px_25px] flex flex-col items-start gap-[5px]">
+      <div className="navbar border-[1px] fixed md:top-0 md:left-0 lg:w-[260px] flex md:flex-col justify-between md:h-screen w-full md:w-auto bottom-0    ">
+        <ul className=" md:p-[40px_25px_5px_25px] flex md:flex-col items-start gap-[5px] justify-evenly w-full md:w-auto">
           <a
             href="#"
-            className="items-center gap-2 p-[5px_5px]  text-[18px]  w-[100%] rounded-[10px] grid grid-cols-[40px_auto]"
+            className="items-center gap-2 p-[5px_5px]  text-[18px]  lg:w-[100%] rounded-[10px] md:grid lg:grid-cols-[40px_auto] hidden"
           >
             <li className="mb-[40px] flex lg:hidden">
               <BsInstagram className="text-[26px]" />
             </li>
-            <p className="fontLogo text-[28px] hidden lg:flex">Instogran</p>
+            <p className="fontLogo text-[28px] hidden lg:flex">Instagran</p>
           </a>
 
           <a
             href=""
-            className=" items-center gap-2 p-[10px_5px]  text-[18px] hover:bg-[#ccc] w-[100%] rounded-[10px] grid grid-cols-[40px_auto]"
+            className=" items-center gap-2 p-[10px_5px]  text-[18px] hover:bg-[#f1f2f5] lg:w-[100%] rounded-[10px] grid lg:grid-cols-[40px_auto]"
           >
             <GoHomeFill className="text-[30px]" />
-            <li className="hidden lg:flex">Home</li>
+            <li className="hidden lg:flex text-[18px]">Home</li>
           </a>
           <a
             href=""
-            className=" items-center gap-2 p-[10px_5px]  text-[20px] hover:bg-[#ccc] w-[100%] rounded-[10px] grid grid-cols-[40px_auto]"
+            className=" items-center gap-2 p-[10px_5px]  text-[20px] hover:bg-[#f1f2f5] lg:w-[100%] rounded-[10px] grid lg:grid-cols-[40px_auto]"
           >
             <BiSearch className="text-[30px]" />
-            <li className="hidden lg:flex">Search</li>
+            <li className="hidden lg:flex text-[18px]">Search</li>
           </a>
           <a
             href=""
-            className=" items-center gap-2 p-[10px_5px] text-[20px] hover:bg-[#ccc] w-[100%] rounded-[10px] grid grid-cols-[40px_auto]"
+            className="hidden  items-center gap-2 p-[10px_5px] text-[20px] hover:bg-[#f1f2f5] lg:w-[100%] rounded-[10px] md:grid lg:grid-cols-[40px_auto]"
           >
             <FaRegCompass className="text-[25px] " />
-            <li className="hidden lg:flex">Explore</li>
+            <li className="hidden lg:flex text-[18px]">Explore</li>
           </a>
           <a
             href=""
-            className=" items-center gap-2 p-[10px_5px] text-[20px] hover:bg-[#ccc] w-[100%] rounded-[10px] grid grid-cols-[40px_auto]"
+            className=" items-center gap-2 p-[10px_5px] text-[20px] hover:bg-[#f1f2f5] lg:w-[100%] rounded-[10px] grid lg:grid-cols-[40px_auto]"
           >
             <BiMoviePlay className="text-[28px] " />
-            <li className="hidden lg:flex">Reels</li>
+            <li className="hidden lg:flex text-[18px]">Reels</li>
           </a>
           <a
             href=""
-            className=" items-center gap-2 p-[10px_5px] text-[20px] hover:bg-[#ccc] w-[100%] rounded-[10px] grid grid-cols-[40px_auto]"
+            className=" items-center gap-2 p-[10px_5px] text-[20px] hover:bg-[#f1f2f5] lg:w-[100%] rounded-[10px] grid lg:grid-cols-[40px_auto]"
           >
             <LiaFacebookMessenger className="text-[30px]" />
-            <li className="hidden lg:flex">Messages</li>
+            <li className="hidden lg:flex text-[18px]">Messages</li>
           </a>
           <a
             href=""
-            className=" items-center gap-2 p-[10px_5px] text-[20px] hover:bg-[#ccc] w-[100%] rounded-[10px] grid grid-cols-[40px_auto]"
+            className=" hidden items-center gap-2 p-[10px_5px] text-[20px] hover:bg-[#f1f2f5] lg:w-[100%] rounded-[10px] md:grid lg:grid-cols-[40px_auto]"
           >
             <AiOutlineHeart className="text-[29px]" />
-            <li className="hidden lg:flex">Natificantions</li>
+            <li className="hidden lg:flex text-[18px]">Natificantions</li>
           </a>
           <a
             href=""
-            className=" items-center gap-2 p-[10px_5px] text-[20px] hover:bg-[#ccc] w-[100%] rounded-[10px] grid grid-cols-[40px_auto]"
+            className="hidden items-center gap-2 p-[10px_5px] text-[20px] hover:bg-[#f1f2f5] lg:w-[100%] rounded-[10px] md:grid lg:grid-cols-[40px_auto]"
           >
             <AiOutlinePlusSquare className="text-[28px]" />
-            <li className="hidden lg:flex">Create</li>
+            <li className="hidden lg:flex text-[18px]">Create</li>
           </a>
           <a
             href=""
-            className=" items-center gap-2 p-[10px_5px] text-[20px] hover:bg-[#ccc] w-[100%] rounded-[10px] grid grid-cols-[40px_auto]"
+            className=" items-center gap-2 p-[10px_5px] text-[20px] hover:bg-[#f1f2f5] lg:w-[100%] rounded-[10px] grid lg:grid-cols-[40px_auto]"
           >
-            <img src={profile} alt="" className="rounded-full w-[32px]  border-[2px] border-[#ccc]" />
+            <img
+              src={profile}
+              alt=""
+              className="rounded-full w-[32px]  border-[2px] border-[#ccc]"
+            />
 
-            <li className="hidden lg:flex">Profile</li>
+            <li className="hidden lg:flex text-[18px]">Profile</li>
           </a>
         </ul>
-        <div className="p-[5px_25px]">
+        <div className="p-[5px_25px] hidden md:flex">
           <a
             href=""
-            className=" items-center gap-1 p-[10px_5px] text-[20px] hover:bg-[#ccc] w-[100%] rounded-[10px] grid grid-cols-[40px_auto]"
+            className=" items-center gap-1 p-[10px_5px] text-[20px] hover:bg-[#f1f2f5] lg:w-[100%] rounded-[10px] grid lg:grid-cols-[40px_auto]"
           >
             <AiOutlineMenu className="text-[28px]" />
             <li className="hidden lg:flex">More</li>
